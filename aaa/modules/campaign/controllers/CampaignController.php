d0d357071ebc8f3e6e35d0979a54457e9afd1b77
ShokoKondo <ybuchi3168@gmail.com>
2015-12-17 21:15:47 +0900

    initialize commit

---
+++ aaa/modules/campaign/controllers/CampaignController.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by IntelliJ IDEA.
+ * User: shoko
+ * Date: 2015/12/14
+ * Time: 15:58
+ */
+ class CampaignController {
+    function index() {
+        waaaa();
+    }
+    function waaaa() {
+        return "";
+    }
+ }
